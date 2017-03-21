405fa3349d69f5b37f0e40cd53db079ef0c5a35c
phyng <phyngk@gmail.com>
2017-03-21 09:17:25 +0800

    add rekapi-timeline to local

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 grunt build
 rm -rf ../mantra_dist
 cp -R dist ../mantra_dist
+mkdir -p ../mantra_dist/node_modules/stylie/app/img
+cp app/scripts/stylie/img/circle.png ../mantra_dist/node_modules/stylie/app/img/
